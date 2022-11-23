7a4b3815ffea047e067203d80766b7feccde0f6c
Ing. ABV <alexanderbarajas@gmail.com>
2022-11-22 23:27:09 -0500

    creacion del fronted del proyecto, página de inicio

--- App.tsx
+++ App.tsx
@@ -1,11 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View, Image, Text, TextInput, Button, ToastAndroid } from 'react-native';
+import { RoundedButton } from './src/components/RoundedButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/chef.jpg')}
+        style={styles.imageBackGround}
+      />
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('./assets/logo.png')}
+          style={styles.logoImage}
+        />
+        <Text style={styles.logoText}>
+          FOOD APP
+        </Text>
+      </View>
+
+      <View style={styles.form}>
+        <Text style={styles.formText}>INGRESAR</Text>
+        <View style={styles.formInput}>
+          <Image 
+            source={require('./assets/email.png')}
+            style={styles.formIcon}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder='Correo Electrónico'
+            keyboardType='email-address'
+
+          />
+        </View> 
+
+        <View style={styles.formInput}>
+          <Image 
+            source={require('./assets/password.png')}
+            style={styles.formIcon}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder='Contraseña'
+            keyboardType='default'
+            secureTextEntry={true}
+
+          />
+        </View>
+
+        <View
+          style={{marginTop:30}}
+        >
+          <RoundedButton text='ENTRAR' onPress={()=>ToastAndroid.show('HOLAAAAA', ToastAndroid.LONG)}/>
+        </View>
+        <View style={styles.formRegister}>
+          <Text>No tienes cuenta?</Text>
+          <Text style={styles.formRegisterText}>Registrate</Text>
+        </View>
+        
+      </View>
     </View>
   );
 }
@@ -13,8 +66,70 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black'
   },
+  imageBackGround:{
+    width: '100%',
+    height:'100%',
+    opacity: 0.7,
+    bottom:'30%'
+  },
+  form: {
+    width: '100%',
+    height: '40%',
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    padding: 30
+},
+  formText: {
+      fontWeight: 'bold',
+      fontSize: 16
+  },
+  formIcon: {
+      width: 25,
+      height: 25,
+      marginTop: 5
+  },
+  formInput: {
+      flexDirection: 'row',
+      marginTop: 30,
+  },
+  formTextInput: {
+      flex: 1,
+      borderBottomWidth: 1,
+      borderBottomColor: '#AAAAAA',
+      marginLeft: 15
+  },
+  formRegister: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      marginTop: 30
+  },
+  formRegisterText: {
+      fontStyle: 'italic',
+      color: 'orange',
+      borderBottomWidth: 1,
+      borderBottomColor: 'orange',
+      fontWeight: 'bold',
+      marginLeft: 10
+  },
+  logoContainer: {
+      position: 'absolute',
+      alignSelf: 'center',
+      top: '15%'
+  },
+  logoImage: {
+      width: 100,
+      height: 100
+  },
+  logoText: {
+      color: 'white',
+      textAlign: 'center',
+      fontSize: 20,
+      marginTop: 10,
+      fontWeight: 'bold'
+  }
 });
